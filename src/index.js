9ebb326baabef8f657312874c24a4ab3e1fc7fa6
Adrien Bélanger <adribelanger@gmail.com>
2025-08-08 16:12:40 -0400

    Revert last 10 pull requests

--- src/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('start-btn').addEventListener('click', () => {
-  document.body.classList.add('fade-out');
-  document.body.addEventListener('animationend', () => {
-    const target = 'page1.html' + location.hash;
-    window.location.href = target;
-  }, { once: true });
-});
